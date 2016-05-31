295de93d12dc61f8ad79fddc39f5ac789ff018c2
Agroscopik <hlehoux@repair.nc>
2016-05-31 17:54:20 +1100

    Ajouter les extensions Behatdans behat.yml

---
+++ features/authentication.feature
@@ -0,0 +1,4 @@
+Feature: Authentication
+  In order to gain access to the farm management area
+  As a farmer
+  I need to be able to login and logout
--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -4,11 +4,11 @@ use Behat\Behat\Context\Context;
 use Behat\Behat\Context\SnippetAcceptingContext;
 use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\TableNode;
-
+use \Behat\MinkExtension\Context\RawMinkContext;
 /**
  * Defines application features from the specific context.
  */
-class FeatureContext implements Context, SnippetAcceptingContext
+class FeatureContext extends RawMinkContext implements Context, SnippetAcceptingContext
 {
     /**
      * Initializes context.
