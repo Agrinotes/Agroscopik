2bb6db4652a32cc194049a66827042938fda88da
Agroscopik <hlehoux@repair.nc>
2016-06-10 15:58:37 +1100

    La création d'intervention est fonctionnelle

---
+++ src/AppBundle/Controller/ActionController.php
@@ -0,0 +1,179 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use AppBundle\Entity\Action;
+use AppBundle\Form\ActionType;
+use Symfony\Component\Security\Acl\Domain\ObjectIdentity;
+use Symfony\Component\Security\Acl\Domain\UserSecurityIdentity;
+use Symfony\Component\Security\Acl\Permission\MaskBuilder;
+
+/**
+ * Action controller.
+ *
+ * @Route("/")
+ */
+class ActionController extends Controller
+{
+    /**
+     * Lists all Action entities.
+     *
+     * @Route("/cropcycle/{id}/action", name="action_index")
+     * @Method("GET")
+     */
+    public function indexAction(Request $request, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $cropCycle = $this->getDoctrine()->getManager()->getRepository('AppBundle:CropCycle')->find($id);
+
+        $actions = $em->getRepository('AppBundle:Action')->findAll();
+
+        return $this->render('@App/action/index.html.twig', array(
+            'actions' => $actions,
+            'cropCycle' => $cropCycle,
+        ));
+    }
+
+    /**
+     * Creates a new Action entity.
+     *
+     * @Route("/cropcycle/{id}/action/new", name="action_new")
+     * @Method({"GET", "POST"})
+     */
+    public function newAction(Request $request, $id)
+    {
+        $action = new Action();
+
+        // Get current cropCycle
+        $cropCycle = $this->getDoctrine()->getManager()->getRepository('AppBundle:CropCycle')->find($id);
+        $cropCycle->addAction($action); // Which also setCropcycle($this) on $action
+
+        // Get Entity Manager
+        $em = $this->getDoctrine()->getManager();
+
+        $form = $this->createForm('AppBundle\Form\ActionType', $action);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($action);
+            $em->flush();
+
+            // Call ACL service
+            $aclProvider = $this->get('security.acl.provider');
+
+            // Create the ACL for current Action $action
+            $objectIdentity = ObjectIdentity::fromDomainObject($action);
+            $acl = $aclProvider->createAcl($objectIdentity);
+
+            // Retrieve the security identity of the current user
+            $securityIdentity = UserSecurityIdentity::fromAccount($this->getUser());
+
+            // Create Access Mask
+            $builder = new MaskBuilder();
+            $builder
+                ->add('view')
+                ->add('edit')
+                ->add('delete');
+            $mask = $builder->get();
+
+            // Insert Object Access Entry
+            $acl->insertObjectAce($securityIdentity, $mask);
+
+            // Update ACL
+            $aclProvider->updateAcl($acl);
+
+            return $this->redirectToRoute('action_show', array('id' => $action->getId()));
+        }
+
+        return $this->render('@App/action/new.html.twig', array(
+            'cropCycle' => $cropCycle,
+            'action' => $action,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a Action entity.
+     *
+     * @Route("/action/{id}", name="action_show")
+     * @Method("GET")
+     */
+    public function showAction(Action $action)
+    {
+        $deleteForm = $this->createDeleteForm($action);
+
+        return $this->render('@App/action/show.html.twig', array(
+            'action' => $action,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing Action entity.
+     *
+     * @Route("/action/{id}/edit", name="action_edit")
+     * @Method({"GET", "POST"})
+     */
+    public function editAction(Request $request, Action $action)
+    {
+        $deleteForm = $this->createDeleteForm($action);
+        $editForm = $this->createForm('AppBundle\Form\ActionType', $action);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($action);
+            $em->flush();
+
+            return $this->redirectToRoute('action_show', array('id' => $action->getId()));
+        }
+
+        return $this->render('@App/action/edit.html.twig', array(
+            'action' => $action,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a Action entity.
+     *
+     * @Route("/action/{id}", name="action_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, Action $action)
+    {
+        $form = $this->createDeleteForm($action);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($action);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('action_index');
+    }
+
+    /**
+     * Creates a form to delete a Action entity.
+     *
+     * @param Action $action The Action entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(Action $action)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('action_delete', array('id' => $action->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+        ;
+    }
+}
