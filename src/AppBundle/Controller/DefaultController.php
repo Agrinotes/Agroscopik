5341efd8b96d6623f390a774ef2ab6734660ab8a
Agroscopik <hlehoux@repair.nc>
2016-07-13 06:45:08 +1100

    La fonction de recherche de lieu sur une carte n'a pas pu être installée. En tapant entrer pour chercher le lieu, le formulaire de création de parcelle est envoyé. J'ai ouvert une discussion sur L.GeoSearch sur GitHub pour demander aux développeurs le meilleur moyen de gérer ce problème.

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -14,6 +14,6 @@ class DefaultController extends Controller
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('AppBundle:default:index.html.twig');
+        return $this->redirectToRoute('fos_user_registration_register');
     }
 }
--- web/app_dev.php
+++ web/app_dev.php
@@ -10,13 +10,15 @@ use Symfony\Component\Debug\Debug;
 
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
+/*
 if (isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-    || !(in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', 'fe80::1', '::1','175.158.185.34']) || php_sapi_name() === 'cli-server')
+    || !(in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', 'fe80::1', '::1','175.158.185.34','115.126.179.214']) || php_sapi_name() === 'cli-server')
 ) {
     header('HTTP/1.0 403 Forbidden');
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
 }
+*/
 
 /**
  * @var Composer\Autoload\ClassLoader $loader
