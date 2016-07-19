a69fa5a8dc2708e9a79c9006b62ca1dc60dbc428
Agroscopik <hlehoux@repair.nc>
2016-07-19 20:47:53 +1100

    Les modifications du stock des phytos inclut les unités de volume et de poids. L'évaluation du stock total n'est pas encore faîte

--- src/AppBundle/Form/FarmSpecialityMvtType.php
+++ src/AppBundle/Form/FarmSpecialityMvtType.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Form;
 
 use Doctrine\DBAL\Types\FloatType;
+use Doctrine\ORM\EntityRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
@@ -10,6 +11,8 @@ use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class FarmSpecialityMvtType extends AbstractType
@@ -20,6 +23,8 @@ class FarmSpecialityMvtType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+
+
         $builder
             ->add('category', EntityType::class, array(
                 'class' => 'AppBundle:FarmSpecialityMvtCategory',
@@ -43,11 +48,23 @@ class FarmSpecialityMvtType extends AbstractType
                 'required' => true,
                 'multiple' => false,
                 'expanded' => false,
+                'query_builder' => function (EntityRepository $er) {
+                    return $er->createQueryBuilder('u')
+                        ->join('u.unitCategory', 'cat')
+                        ->addSelect('cat')
+                        ->where('cat.slug = :slug')
+                        ->setParameter('slug','mass')
+                        ->orWhere('cat.slug = :slug2')
+                        ->setParameter('slug2','volume')
+
+                        ;
+                },
             ))
-            ->add('pricePerUnit',NumberType::class,array(
+            ->add('pricePerUnit',NumberType::class, array(
                 'label'=>'Entrer le prix correspondant',
                 'attr' => array('class' => 'form-control','placeholder'=>'0.00'),
             ));
+
     }
 
     /**
